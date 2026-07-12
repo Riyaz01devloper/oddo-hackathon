86885f8f36cbb762cb7a33a7f011b95b54391c83
Shubham <shubhamgupta7642@gmail.com>
2026-07-12 12:13:03 +0530

    feat: add JWT verification and role authorization middlewares

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const asyncHandler = require("../utils/asyncHandler");
+const ApiError = require("../utils/ApiError");
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized Request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decodedToken.id).select("-password");
+
+    if (!user) {
+        throw new ApiError(401, "Invalid Token");
+    }
+
+    req.user = user;
+
+    next();
+});
+
+module.exports = verifyJWT;
