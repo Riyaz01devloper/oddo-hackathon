6845276e633c803975fc98596659c5e7802c80ad
Riyaz <riyazmalik2046@gmail.com>
2026-09-22 10:56:33 +0530

    Fix production CORS origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://oddo-hackathon-zeta.vercel.app",
+  origin: "https://oddo-hackathon-p9c4uq20g-riyaz-maliks-projects.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
